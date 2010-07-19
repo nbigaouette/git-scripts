7cb93ca8b5020dd6cbb3111dee23c0cc2349418f
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:10:43 -0400

    Verify if no arguments are present, and call help function.

--- git-create-remote-branch.sh
+++ git-create-remote-branch.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+help() {
+    echo "Create a new branch on both local and remote repositories."
+}
+
+if [[ -z "$1" ]]; then
+    help
+    echo "Usage:"
+    echo "$ `basename $0` branch_name"
+    exit
+fi
+
 RemoteBranch="$1"
 echo "Creating remote branch $RemoteBranch..."
 
