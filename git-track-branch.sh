143b81c44d484f463a112df1e8cb6dcf6ea8e324
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:23:17 -0400

    Better colors and log() function.

--- git-track-branch.sh
+++ git-track-branch.sh
@@ -5,8 +5,12 @@ help() {
 }
 
 error() {
-    echo -e "\e[31;1mERROR: $@"
+    echo -e "${col_r}ERROR: $@${col_n}"
     exit
+}
+
+log() {
+    echo -e "${col_b}$@${col_n}"
 }
 
 if [[ -z "$1" ]]; then
@@ -19,8 +23,19 @@ if [[ -z "$1" ]]; then
 fi
 
 RemoteBranch="$1"
-echo "Checking out branch $RemoteBranch..."
 
-git branch --track $RemoteBranch origin/$RemoteBranch
-git checkout $RemoteBranch
+col_b="\e[34;1m"
+col_r="\e[31;1m"
+col_g="\e[32;1m"
+col_n="\e[0m"
+
+log "Creating local branch ${col_g}${RemoteBranch}${col_b} to track remote branch..."
+git branch --track $RemoteBranch origin/$RemoteBranch \
+    || error "Creation of local tracking branch ${col_g}${RemoteBranch}${col_b} failed!"
+
+log "Switching to local branch ${col_g}${RemoteBranch}${col_b}..."
+git checkout $RemoteBranch \
+    || error "Checkout of local branch ${col_g}${RemoteBranch}${col_b} failed!"
+
+log "Updated list of all branches:"
 git branch -a
