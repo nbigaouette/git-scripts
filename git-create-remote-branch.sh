eaf7caaafe3198c3cfc48550d614dfbeb8b74eab
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:08:38 -0400

    Bug fix: correctly push to "origin origin:refs/...", not "implicit"

--- git-create-remote-branch.sh
+++ git-create-remote-branch.sh
@@ -3,7 +3,7 @@
 RemoteBranch="$1"
 echo "Creating remote branch $1..."
 
-git push origin implicit:refs/heads/$RemoteBranch
+git push origin origin:refs/heads/$RemoteBranch
 git fetch origin
 git branch --track $RemoteBranch origin/$RemoteBranch
 git checkout $RemoteBranch
