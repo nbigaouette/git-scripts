64187cb6c51a777a76727b146a844df7d19f5add
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:42:16 -0400

    GPL v3

--- git-delete-remote-branch.sh
+++ git-delete-remote-branch.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright 2009 Nicolas Bigaouette <nbigaouette @t gmail com>
+# This file is part of git-scripts.
+# http://github.com/nbigaouette/git-scripts
+#
+# git-scripts is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# git-scripts is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with git-scripts. If not, see <http://www.gnu.org/licenses/>.
+
 help() {
     echo "Delete a remote branch and its local tracking branch, if present."
 }
