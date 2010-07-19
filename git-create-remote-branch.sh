3c99e4d9d87844fa22a8ee88eacf86e399a0c347
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:25:44 -0400

    Colors and log() function.

--- git-create-remote-branch.sh
+++ git-create-remote-branch.sh
@@ -5,23 +5,41 @@ help() {
 }
 
 error() {
-    echo -e "\e[31;1mERROR: $@"
+    echo -e "${col_r}ERROR: $@${col_n}"
     exit
+}
+
+log() {
+    echo -e "${col_b}$@${col_n}"
 }
 
 if [[ -z "$1" ]]; then
     help
     echo "Usage:"
     echo "$ `basename $0` branch_name"
+
+
     exit
 fi
 
 RemoteBranch="$1"
-echo "Creating remote branch $RemoteBranch..."
 
-git push origin origin:refs/heads/$RemoteBranch
-git fetch origin
-#git branch --track $RemoteBranch origin/$RemoteBranch
-#git checkout $RemoteBranch
-git checkout --track -b $RemoteBranch origin/$RemoteBranch
+col_b="\e[34;1m"
+col_r="\e[31;1m"
+col_g="\e[32;1m"
+col_n="\e[0m"
+
+log "Creating remote branch ${col_g}${RemoteBranch}${col_b}..."
+git push origin origin:refs/heads/$RemoteBranch \
+    || error "Creating remote branch ${col_g}${RemoteBranch}${col_b} failed!"
+
+log "Fetching origin..."
+git fetch origin \
+    || error "Fetching origin failed!"
+
+log "Creating and switching to local branch ${col_g}${RemoteBranch}${col_b}..."
+git checkout --track -b $RemoteBranch origin/$RemoteBranch \
+    || error "Creating and switching to local branch ${col_g}${RemoteBranch}${col_b} failed!"
+
+log "Updated list of all branches:"
 git branch -a
