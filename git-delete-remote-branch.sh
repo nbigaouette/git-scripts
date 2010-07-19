5ec4ea485d32e527903ed9f031cd6d802f77e4e6
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:21:37 -0400

    Better colors and log function.

--- git-delete-remote-branch.sh
+++ git-delete-remote-branch.sh
@@ -5,26 +5,35 @@ help() {
 }
 
 error() {
-    echo -e "\e[31;1mERROR: $@"
+    echo -e "${col_r}ERROR: $@${col_n}"
     exit
+}
+
+log() {
+    echo -e "${col_b}$@${col_n}"
 }
 
 if [[ -z "$1" ]]; then
     help
     echo "Usage:"
     echo "$ `basename $0` branch_name"
+
+
     exit
 fi
 
 RemoteBranch="$1"
 
-b="\e[34;1m"
+col_b="\e[34;1m"
+col_r="\e[31;1m"
+col_g="\e[32;1m"
+col_n="\e[0m"
 
-echo -e "${b}Suppression of local branch ${RemoteBranch}...\e[0m"
-git branch -d ${RemoteBranch} || error "Suppression of local branch failed!"
+log "Suppression of local branch ${col_g}${RemoteBranch}${col_b}..."
+git branch -d ${RemoteBranch} || error "Suppression of local branch ${col_g}${RemoteBranch}${col_r} failed!"
 
-echo -e "${b}Suppression of remote branch ${RemoteBranch}...\e[0m"
+log "Suppression of remote branch ${col_g}${RemoteBranch}${col_b}..."
 git push origin :heads/${RemoteBranch} || error "Suppression of remote branch failed!"
 
-echo -e "${b}Updated list of all branches:\e[0m"
+log "Updated list of all branches:"
 git branch -a
