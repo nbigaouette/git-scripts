fbebf5ab2a089a7e0e65e5c6450997a06df0d01f
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-19 15:09:58 -0400

    Create branch and checkout in one call.

--- git-create-remote-branch.sh
+++ git-create-remote-branch.sh
@@ -5,5 +5,6 @@ echo "Creating remote branch $RemoteBranch..."
 
 git push origin origin:refs/heads/$RemoteBranch
 git fetch origin
-git branch --track $RemoteBranch origin/$RemoteBranch
-git checkout $RemoteBranch
+#git branch --track $RemoteBranch origin/$RemoteBranch
+#git checkout $RemoteBranch
+git checkout --track -b $RemoteBranch origin/$RemoteBranch
